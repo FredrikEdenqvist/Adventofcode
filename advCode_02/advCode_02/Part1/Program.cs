0ef98d6810a5d14660076d5e88287e6ce5ce84fd
Fredrik Edenqvist <fredrik.edenqvist@webstep.se>
2021-12-02 15:57:10 +0100

    Day 2

---
+++ advCode_02/advCode_02/Part1/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] input;
+
+using (FileStream fs = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"), FileMode.Open))
+using (MemoryStream mem = new MemoryStream())
+{
+    fs.CopyTo(mem);
+    input = System.Text.Encoding.UTF8
+        .GetString(mem.ToArray())
+        .Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+}
+
+const string FORWARD = "forward";
+const string UP = "up";
+const string DOWN = "down";
+
+IEnumerable<SubmarinPosition> wayfinder = input.Select(x =>
+{
+    if (x.StartsWith(FORWARD))
+        return new SubmarinPosition(int.Parse(x.Replace(FORWARD, string.Empty).Replace(" ", string.Empty)), 0);
+    if (x.StartsWith(UP))
+        return new SubmarinPosition(0, -1 * int.Parse(x.Replace(UP, string.Empty).Replace(" ", string.Empty)));
+    if (x.StartsWith(DOWN))
+        return new SubmarinPosition(0, int.Parse(x.Replace(DOWN, string.Empty).Replace(" ", string.Empty)));
+
+    return SubmarinPosition.Empty;
+
+}).ToList();
+
+SubmarinPosition sumPart1 = wayfinder.Aggregate((p1, p2) => p1.Add(p2));
+SubmarinPosition sumPart2 = wayfinder.Aggregate((p1, p2) => p1.AddWithAim(p2));
+
+Console.WriteLine($"Part 1 Product is { sumPart1.Product }");
+Console.WriteLine($"Part 2 Product is { sumPart2.Product }");
---
+++ advCode_02/advCode_02/Part1/SubmarinPosition.cs
@@ -0,0 +1,50 @@
+﻿public class SubmarinPosition
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Aim { get; set; }
+
+    public SubmarinPosition()
+    {
+        X = 0;
+        Y = 0;
+        Aim = 0;
+    }
+
+    public SubmarinPosition(int x, int y, int aim = 0)
+    {
+        X = x;
+        Y = y;
+        Aim = aim;
+    }
+    public int Product => X * Y;
+    public static SubmarinPosition Empty => new SubmarinPosition();
+
+    public SubmarinPosition Add(SubmarinPosition newPosition)
+    {
+        return new SubmarinPosition
+        {
+            X = X + newPosition.X,
+            Y = Y + newPosition.Y
+        };
+    }
+
+    public SubmarinPosition AddWithAim(SubmarinPosition newPosition)
+    {
+        if (newPosition.X > 0)
+            return new SubmarinPosition
+            {
+                X = X + newPosition.X,
+                Y = Y + (newPosition.X * Aim),
+                Aim = Aim
+            };
+
+        return new SubmarinPosition
+        {
+            X = X,
+            Y = Y,
+            Aim = Aim + newPosition.Y
+        };
+    }
+    
+}
