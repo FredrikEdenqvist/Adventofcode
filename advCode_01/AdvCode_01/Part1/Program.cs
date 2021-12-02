c92ad8577352cfc83659dd68315681d9b056a2fb
Fredrik Edenqvist <fredrik.edenqvist@webstep.se>
2021-12-02 15:56:53 +0100

    Day 1

---
+++ advCode_01/AdvCode_01/Part1/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] input;
+
+using (FileStream fs = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"), FileMode.Open))
+using (MemoryStream mem = new MemoryStream())
+{
+    fs.CopyTo(mem);
+    input = System.Text.Encoding.UTF8
+        .GetString(mem.ToArray())
+        .Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(x => int.Parse(x))
+        .ToArray();
+}
+
+Console.WriteLine($"Total count: {input.Zip(input[1..], (a, b) => a < b).Count(x => x)}");
---
+++ advCode_01/Part2/Program.cs
@@ -0,0 +1,21 @@
+﻿IList<int> input;
+
+using (FileStream fs = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"), FileMode.Open))
+using (MemoryStream mem = new MemoryStream())
+{
+    fs.CopyTo(mem);
+    input = System.Text.Encoding.UTF8
+        .GetString(mem.ToArray())
+        .Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(x => int.Parse(x))
+        .ToList();
+}
+
+var stage1 = new List<int>();
+for (int i = 0; i < input.Count - 2; i++)
+{
+    stage1.Add(input[i] + input[i + 1] + input[i + 2]);
+}
+
+var arr = stage1.ToArray();
+Console.WriteLine($"Total altsum: {arr.Zip(arr[1..], (a, b) => a < b).Count(x => x)}");
