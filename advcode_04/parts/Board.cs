a97634aa03954bff64e8f5c42c0e27e3c2ecb584
Fredrik Edenqvist <fredrik.edenqvist@webstep.se>
2021-12-05 22:05:36 +0100

    Day 4 part 1 wip

---
+++ advcode_04/parts/Board.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parts
+{
+    public class Board
+    {
+        public int[] Sequence { get; }
+        public int X { get; }
+        public int Y { get; }
+
+        public static IEnumerable<Board> GenerateBoards(int[] boardSequence, int x, int y)
+        {
+            var sequenceLenght = x * y;
+            var boards = boardSequence.Length / sequenceLenght;
+            for(int i = 0; i < boards; i++)
+            {
+                var start = i * sequenceLenght;
+                var end = start + sequenceLenght;
+                yield return new Board(boardSequence[start..end], x, y);
+            }
+        }
+
+        public Board(int[] sequence, int x, int y)
+        {
+            Sequence = sequence;
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ advcode_04/parts/Program.cs
@@ -0,0 +1,23 @@
+﻿string input;
+
+using (FileStream fs = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"), FileMode.Open))
+using (MemoryStream mem = new MemoryStream())
+{
+    fs.CopyTo(mem);
+    input = System.Text.Encoding.UTF8.GetString(mem.ToArray());
+}
+int endpositionRow1 = input.IndexOf('\n');
+
+int[] bingonumbers = input[..endpositionRow1]
+    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => Convert.ToInt32(x))
+    .ToArray();
+
+int[] bingoboards = input[endpositionRow1..]
+    .Split(new [] { " ", "\n" }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => Convert.ToInt32(x))
+    .ToArray();
+
+var boards = parts.Board.GenerateBoards(bingoboards, 5, 5).ToArray();
+
+Console.WriteLine("");
