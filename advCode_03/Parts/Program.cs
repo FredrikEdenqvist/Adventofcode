f0a8cbec89fd29ac88a2f189c2bbf99fa213ee23
Fredrik Edenqvist <fredrik.edenqvist@webstep.se>
2021-12-05 20:53:58 +0100

    Ugly solution for day 3 part 2

--- advCode_03/Parts/Program.cs
+++ advCode_03/Parts/Program.cs
@@ -11,6 +11,50 @@ using (MemoryStream mem = new MemoryStream())
 }
 int stringlength = input[0].Length;
 
+(int zeros, int ones) CountOn(string[] input, int position)
+{
+    int zeros = 0;
+    int ones = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        var value = Convert.ToInt32(input[i][position].ToString()) == 0;
+        zeros += (value ? 1 : 0);
+        ones += (value ? 0 : 1);
+    }
+    return (zeros, ones);
+}
+
+string[] GetOnlyValuesAtPosition(string[] input, int value, int position)
+{
+    return input.Where(item => Convert.ToInt32(item[position].ToString()) == value).ToArray();
+}
+
+string GetOxygenRate(string[] input, int length)
+{
+    string[] currentSet = input;
+    for(int i = 0; i < length; i++)
+    {
+        (int zeros, int ones) = CountOn(currentSet, i);
+        currentSet = GetOnlyValuesAtPosition(currentSet, (zeros > ones ? 0 : 1), i);
+        if (currentSet.Length == 1) return currentSet[0];
+    }
+
+    return "NA";
+}
+
+string GetCO2ScrubberRate(string[] input, int length)
+{
+    string[] currentSet = input;
+    for (int i = 0; i < length; i++)
+    {
+        (int zeros, int ones) = CountOn(currentSet, i);
+        currentSet = GetOnlyValuesAtPosition(currentSet, (zeros <= ones ? 0 : 1), i);
+        if (currentSet.Length == 1) return currentSet[0];
+    }
+
+    return "NA";
+}
+
 var results = input.Aggregate(new (int zeros, int ones)[stringlength], (aggr, item) =>
 {
     for (int i = 0; i < item.Length; i++)
@@ -24,6 +68,7 @@ var results = input.Aggregate(new (int zeros, int ones)[stringlength], (aggr, it
 
 int[] maxValues = new int[stringlength];
 int[] minValues = new int[stringlength];
+bool[] sameValues= new bool[stringlength];
 
 for (int i = 0; i < stringlength; i++)
 {
@@ -42,8 +87,17 @@ int ToDecimal(int[] item)
     return sum;
 }
 
+int StringToDecimal(string item)
+{
+    return ToDecimal(item.Select(x => Convert.ToInt32(x.ToString())).ToArray());
+}
+
 Console.WriteLine($"Minvalue: {string.Join("", minValues.Select(x => $"{x}"))}: {ToDecimal(minValues)}");
 Console.WriteLine($"Maxvalue: {string.Join("", maxValues.Select(x => $"{x}"))}: {ToDecimal(maxValues)}");
 
 Console.WriteLine($"Product for part 1: {ToDecimal(minValues) * ToDecimal(maxValues)}");
 
+Console.WriteLine($"Oxygen rate: {GetOxygenRate(input, stringlength)}");
+Console.WriteLine($"CO2 rate: {GetCO2ScrubberRate(input, stringlength)}");
+
+Console.WriteLine($"Product for part 2: {StringToDecimal(GetOxygenRate(input, stringlength)) * StringToDecimal(GetCO2ScrubberRate(input, stringlength))}");
