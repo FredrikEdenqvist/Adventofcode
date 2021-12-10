7ab6fe6f0b4d431dc72ed3d5dff1540ef0effcc3
Fredrik Edenqvist <fredrik.edenqvist@webstep.se>
2021-12-10 15:14:47 +0100

    Day5 part 1 and 2. not pretty...

---
+++ advcode_05/parts/CountPositionExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace parts
+{
+    public static class CountPositionExtensions
+    {
+        public static int CountPosition(this (int x1, int y1, int x2, int y2)[] list, int x, int y)
+        {
+            return list.Count(item => item.IsPassingThru(x, y));
+        }
+
+        public static bool IsPassingThru(this (int x1, int y1, int x2, int y2) item, int x, int y)
+        {
+            var maxY = item.y1 > item.y2 ? item.y1 : item.y2;
+            var minY = item.y1 > item.y2 ? item.y2 : item.y1;
+            var maxX = item.x1 > item.x2 ? item.x1 : item.x2;
+            var minX = item.x1 > item.x2 ? item.x2 : item.x1;
+
+            if (minY > y || y > maxY || minX > x || x > maxX)
+                return false;
+
+            if (item.x2 == item.x1)
+                return item.x1 == x && minY <= y && y <= maxY;
+
+            decimal k = (item.y2 - item.y1) / (item.x2 - item.x1);
+            decimal m = item.y1 - (k * item.x1);
+
+            return y == (k * x) + m;
+        }
+
+        public static bool Is45Degree(this (int x1, int y1, int x2, int y2) item)
+        {
+            if (item.x2 == item.x1) return false;
+            return 1m == Math.Abs((decimal)(item.y2 - item.y1) / (item.x2 - item.x1));
+        }
+    }
+}
---
+++ advcode_05/parts/Program.cs
@@ -0,0 +1,51 @@
+﻿using parts;
+string input;
+
+using (FileStream fs = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"), FileMode.Open))
+using (MemoryStream mem = new MemoryStream())
+{
+    fs.CopyTo(mem);
+    input = System.Text.Encoding.UTF8.GetString(mem.ToArray());
+}
+
+(int x1, int y1, int x2, int y2)[] directions = input
+    .Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => x.Split(new [] { " -> ", "," }, StringSplitOptions.RemoveEmptyEntries))
+    .Select(x => (int.Parse(x[0]), int.Parse(x[1]), int.Parse(x[2]), int.Parse(x[3])))
+    .ToArray();
+
+var part1Filtered = directions.Where(v => v.x1 == v.x2 || v.y1 == v.y2).ToArray();
+var part2Filtered = directions.Where(v => v.x1 == v.x2 || v.y1 == v.y2 || v.Is45Degree()).ToArray();
+var maxX = new[] { directions.Max(v => v.x1), directions.Max(v => v.x2) }.Max();
+var maxY = new[] { directions.Max(v => v.y1), directions.Max(v => v.y2) }.Max();
+
+
+int FindAllOverlapps(int maxX, int maxY, (int x1, int y1, int x2, int y2)[] source)
+{
+    int[,] grid = new int[maxX, maxY];
+
+    for (int y = 0; y < maxY; y++)
+    {
+        for (int x = 0; x < maxX; x++)
+        {
+            grid[x, y] = source.CountPosition(x, y);
+        }
+    }
+
+    int countMoreIntersec = 0;
+    for (int y = 0; y < maxY; y++)
+    {
+        for (int x = 0; x < maxX; x++)
+        {
+            if (grid[x, y] > 1)
+                countMoreIntersec++;
+        }
+    }
+
+    return countMoreIntersec;
+}
+
+
+
+Console.WriteLine($"Part1 {FindAllOverlapps(maxX, maxY, part1Filtered)} no of overlaps."); // 6564
+Console.WriteLine($"Part2 {FindAllOverlapps(maxX, maxY, part2Filtered)} no of overlaps."); // 19172
