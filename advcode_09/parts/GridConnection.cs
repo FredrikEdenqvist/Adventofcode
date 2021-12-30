36f6d24ca472b7ea96a1aa14de232a7ccecf05c8
Fredrik Edenqvist <fredrik.edenqvist@webstep.se>
2021-12-30 18:24:08 +0100

    Day 9 part 2 naive version

---
+++ advcode_09/parts/GridConnection.cs
@@ -0,0 +1,61 @@
+﻿
+
+namespace parts
+{
+    public class GridConnection
+    {
+        public int Value { get; set; }
+        public (int x, int y) Point { get; set; }
+        public List<GridConnection> Connections { get; set; } = new List<GridConnection>();
+
+        public IList<GridConnection> GetAllBut((int x, int y) p)
+        {
+            return Connections.Where(z => z.Point != p).ToList();
+        }
+
+        public void RemoveItem(GridConnection item)
+        {
+            Connections.Remove(item);
+        }
+    }
+
+    public class GridClusterConnections
+    {
+        private HashSet<GridConnection> currentCluster;
+
+        public GridClusterConnections()
+        {
+            currentCluster = new HashSet<GridConnection>();
+        }
+
+        public List<GridConnection> GetCluster(GridConnection tailThreadConnection)
+        {
+            if (tailThreadConnection == null)
+                throw new ArgumentNullException(nameof(tailThreadConnection), "Parameter cannot be null");
+
+            currentCluster = new HashSet<GridConnection>();
+
+            UnRavel(tailThreadConnection);
+
+            return currentCluster.ToList();
+        }
+
+        private void UnRavel(GridConnection tailThreadConnection)
+        {
+            currentCluster.Add(tailThreadConnection);
+            foreach(var connection in tailThreadConnection.Connections)
+            {
+                connection.RemoveItem(tailThreadConnection);
+            }
+            var list = tailThreadConnection.Connections;
+            tailThreadConnection.Connections = new List<GridConnection>();
+
+            foreach(var connection in list)
+            {
+                if (currentCluster.Contains(connection))
+                    continue;
+                UnRavel(connection);
+            }
+        }
+    }
+}
--- advcode_09/parts/Program.cs
+++ advcode_09/parts/Program.cs
@@ -1,4 +1,5 @@
-﻿string input;
+﻿using parts;
+string input;
 
 using (FileStream fs = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"), FileMode.Open))
 using (MemoryStream mem = new MemoryStream())
@@ -18,7 +19,7 @@ IEnumerable<int> GetSurrundedValues(int[][] grid, int pX, int pY)
     var yMax = grid[0].Length - 1;
     (int x, int y)[] coords = new[] { (pX - 1, pY), (pX + 1, pY), (pX, pY - 1), (pX, pY + 1) };
     return coords
-        .Where(p => p.y >= 0 && p.y <= yMax && 
+        .Where(p => p.y >= 0 && p.y <= yMax &&
                     p.x >= 0 && p.x <= xMax && (p.x != pX || p.y != pY))
         .Select(coord => grid[coord.x][coord.y]);
 }
@@ -38,3 +39,47 @@ IEnumerable<int> GetLowestHeight(int[][] grid)
 }
 
 Console.WriteLine($"Part 1: {GetLowestHeight(grid).Sum(x => x + 1)}");
+
+
+IEnumerable<GridConnection> GenerateConnections(int[][] grid)
+{
+    for (int x = 0; x < grid.Length; x++)
+    {
+        for (int y = 0; y < grid[x].Length; y++)
+        {
+            yield return new GridConnection { Point = (x, y), Value = grid[x][y] };
+        }
+    }
+}
+
+var connections = GenerateConnections(grid)
+    .Where(x => x.Value != 9)
+    .ToDictionary(x => x.Point, x => x);
+
+foreach (var connection in connections)
+{
+    var point = connection.Key;
+    var list = new[] { (point.x, point.y - 1), (point.x, point.y + 1), (point.x - 1, point.y), (point.x + 1, point.y) }.ToHashSet();
+    connection.Value.Connections = connections.Where(x => list.Contains(x.Key)).Select(x => x.Value).ToList();
+}
+
+var connectionManager = new GridClusterConnections();
+var listOfClusters = new List<List<GridConnection>>();
+do
+{
+    var cluster = connectionManager.GetCluster(connections.First().Value);
+    listOfClusters.Add(cluster);
+    foreach (var item in cluster)
+    {
+        var ok = connections.Remove(item.Point);
+    }
+
+} while (connections.Any());
+
+var countclusterHeat = listOfClusters
+    .Select(x => x.Count)
+    .OrderByDescending(x => x)
+    .Take(3)
+    .Aggregate(1, (a, b) => a * b);
+
+Console.WriteLine($"Part 2: {countclusterHeat}");
