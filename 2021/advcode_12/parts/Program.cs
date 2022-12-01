427eab5ebe2f7d1e12d93725349b08ff4e88e733
Fredrik Edenqvist <fredrik.edenqvist@webstep.se>
2022-12-01 10:51:47 +0100

    Creating year folders

---
+++ 2021/advcode_12/parts/Program.cs
@@ -0,0 +1,12 @@
+﻿string input;
+
+using (FileStream fs = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"), FileMode.Open))
+using (MemoryStream mem = new MemoryStream())
+{
+    fs.CopyTo(mem);
+    input = System.Text.Encoding.UTF8.GetString(mem.ToArray());
+}
+
+var list = input.Split(new char[] {'\n'}, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Split(new char[] { '-' }));
+const string start = nameof(start);
+const string end = nameof(end);
