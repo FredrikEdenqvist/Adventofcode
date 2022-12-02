0c01f84f69e500cd0cc66c4287b5223fe15046f7
Fredrik Edenqvist <fredrik.edenqvist@webstep.se>
2022-12-02 08:35:57 +0100

    day 2, navive solution

---
+++ 2022/advcode_02/advcode_02/Program.cs
@@ -0,0 +1,21 @@
+﻿using advcode_02;
+
+string input;
+
+using (FileStream fs = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"), FileMode.Open))
+using (MemoryStream mem = new MemoryStream())
+{
+    fs.CopyTo(mem);
+    input = System.Text.Encoding.UTF8.GetString(mem.ToArray());
+}
+
+var turns = input
+    .Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(Turn.Create)
+    .ToList();
+
+Console.WriteLine("Part 1:");
+Console.WriteLine($"Total score: {turns.Select(x => x.CalculateMyOutcome()).Sum()}");
+
+Console.WriteLine("Part 2:");
+Console.WriteLine($"Total score: {turns.Select(x => x.CalculateMyOutcomeWithSuggestion()).Sum()}");
---
+++ 2022/advcode_02/advcode_02/Turn.cs
@@ -0,0 +1,86 @@
+﻿namespace advcode_02
+{
+    internal enum Shape
+    {
+        Rock = 1,
+        Paper = 2,
+        Scissor = 3
+    }
+
+    internal class Turn
+    {
+        public required Shape OpponentShape { get; init; }
+        public required Shape MyShape { get; init; }
+
+        internal int CalculateMyOutcome()
+        {
+            int score = 0;
+            if (MyShape == Shape.Rock && OpponentShape == Shape.Scissor ||
+                MyShape == Shape.Paper && OpponentShape == Shape.Rock ||
+                MyShape == Shape.Scissor && OpponentShape == Shape.Paper)
+            {
+                score = 6;
+            }
+            if (MyShape == OpponentShape)
+            {
+                score = 3;
+            }
+
+            return (int)MyShape + score;
+        }
+
+        internal int CalculateMyOutcomeWithSuggestion()
+        {
+            int score = 0;
+            Shape NewShape = Shape.Rock;
+
+            switch (MyShape)
+            {
+                case Shape.Rock:
+                    if (OpponentShape == Shape.Rock)
+                        NewShape = Shape.Scissor;
+                    else if (OpponentShape == Shape.Paper)
+                        NewShape = Shape.Rock;
+                    else
+                        NewShape = Shape.Paper;
+                    break;
+                case Shape.Paper:
+                    score = 3;
+                    NewShape = OpponentShape;
+                    break;
+                case Shape.Scissor:
+                    score = 6;
+                    if (OpponentShape == Shape.Rock)
+                        NewShape = Shape.Paper;
+                    else if (OpponentShape == Shape.Paper)
+                        NewShape = Shape.Scissor;
+                    else
+                        NewShape = Shape.Rock;
+                    break;
+                default: 
+                    break;
+            }
+
+            return (int)NewShape + score;
+        }
+
+        internal static Shape GetShape(char shape)
+        {
+            return shape switch
+            {
+                'A' => Shape.Rock,
+                'B' => Shape.Paper,
+                'C' => Shape.Scissor,
+                'X' => Shape.Rock,
+                'Y' => Shape.Paper,
+                'Z' => Shape.Scissor,
+                _ => throw new NotSupportedException()
+            };
+        }
+
+        internal static Turn Create(string turn)
+        {
+            return new Turn { MyShape = GetShape(turn[2]), OpponentShape = GetShape(turn[0]) };
+        }
+    }
+}
